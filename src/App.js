31b5a787159b9107f0aea2cc849a520581ecfe9e
Santanu Maity <santanumaity334@gmail.com>
2025-07-29 15:07:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import {Route, Routes} from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Public/Home";
+import Card from "./Public/Product";
+import Singup from "./Public/Singup";
+function App() {
+  return (
+   <>
+   <Routes>
+    <Route path="/" element = {<> <Navbar/><Home/> <Card/></>}/>
+    <Route path="/Login" element = {<> <Navbar/><Singup/></>}/>
+   </Routes>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Public/Singup.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function Singup() {
+  return (
+    <>
+    <div class="container ">
+      <form>
+        
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">First Name</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Example - Santanu"required/>
+        </div>
+        
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Last Name</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Example - Maity"required/>
+        </div>
+        
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Mobile</label>
+        <input type="Number" class="form-control" id="exampleFormControlInput1" placeholder="+91 xxxxxxxxxx"required/>
+        </div>
+        
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">DOB</label>
+        <input type="Date" class="form-control" id="exampleFormControlInput1" placeholder="DOB" required/>
+        </div>
+
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Gender</label>
+        <select class="form-select" aria-label="Default select example" required>
+        <option selected>Select Your Gender </option>
+        <option value="Male">Male</option>
+         <option value="Female">Female</option>
+        </select>
+        </div>
+
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required/>
+        </div>
+        <button type="submit" class="btn btn-primary">SUBMIT</button>
+      </form>
+
+  
+    </div>
+    
+    </>
+  )
+}
