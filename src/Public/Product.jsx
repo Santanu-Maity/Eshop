53183a1559b57d382e8715bccc563800401d0136
PRITTHISH-MONDAL <mondalpritthish42@gmail.com>
2025-07-29 16:12:53 +0530

    product name

--- src/Public/Product.jsx
+++ src/Public/Product.jsx
@@ -3,15 +3,17 @@ import React from 'react'
 export default function Product() {
   const products=[
     {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
-    {id:1,name:"Speaker",price:1000},
+    {id:2,name:"TV",price:20000},
+    {id:3,name:"Shoe",price:900},
+    {id:4,name:"Smart Phone",price:30000},
+    {id:5,name:"Laptop",price:60000},
+    {id:6,name:"Shirt",price:1000},
+    {id:7,name:"Smart Watch",price:1000},
+    {id:8,name:"Headphone",price:1200},
+    {id:9,name:"Wall Clock",price:700},
+    {id:10,name:"Cycle",price:2500},
+    {id:11,name:"Shampoo",price:800},
+    {id:12,name:"Perfume",price:900},
 
     
   ]
