760f9f356f88f6206ffb39f0037ff7d0c487aef1
Suman Mitra <154526708+sumanmitra00@users.noreply.github.com>
2025-07-30 01:56:34 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Public/Home";
-
 import Singup from "./Public/Singup";
 import Product from "./Public/Product";
 import Login from "./Public/Login";
-import Details from "./Public/Details";
 
 
 
 
 function App() {
   return (
-   <>
-   <Routes>
-    <Route path="/" element = {<> <Navbar/><Home/> </>}/>
-    <Route path="/Signup" element = {<> <Navbar/><Singup/></>}/>
-    <Route path="/Product" element = {<> <Navbar/><Product/></>}/>
-    <Route path="/Login" element = {<> <Navbar/><Login/></>}/>
-     <Route path="/Details" element = {<> <Navbar/><Details/></>}/>
+    <>
+      <Routes>
+        <Route path="/" element={<> <Navbar /><Home /> </>} />
+        <Route path="/Signup" element={<> <Navbar /><Singup /></>} />
+        <Route path="/Product" element={<> <Navbar /><Product /></>} />
+        <Route path="/Login" element={<> <Navbar /><Login /></>} />
+        
 
-   </Routes>
-   </>
+      </Routes>
+    </>
   );
 }
 
