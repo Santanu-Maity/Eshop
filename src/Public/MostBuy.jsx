d66ba26b26ae38a39768f2141f41854816a65139
PRITTHISH-MONDAL <mondalpritthish42@gmail.com>
2025-07-30 15:40:33 +0530

    update

--- src/Public/MostBuy.jsx
+++ src/Public/MostBuy.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 
 export default function MostBuy() {
   const products = [
-    { id: 11, name: "Buds", price: 300, image: './img/buds.jpg' },
-    { id: 12, name: "Cycle", price: 5202, image: './img/cycle.jpg' },
-    { id: 13, name: "TV", price: 18999, image: './img/TV.jpg' },
-    { id: 14, name: "Neck band", price: 699, image: './img/neck band.jpg' },
+    { id: 11, name: "Washing Machine", price: 6490, image: './img/washing machine.jpg' },
+    { id: 12, name: "Neck band", price: 699, image: './img/neck band.jpg' },
+   
+    
+    { id: 13, name: "Smart Watch", price: 1299, image: './img/smart Watch.jpg' },
+     { id: 14, name: "Buds", price: 1399, image: './img/buds.jpg' },
     { id: 15, name: "Laptop", price: 49500, image: './img/laptop.jpg' },
-    { id: 16, name: "T-Shirt", price: 398, image: './img/t-shirt2.jpg' },
+    { id: 16, name: "T-Shirt", price: 398, image: './img/t-shirt.jpg' },
     
 
   ]
